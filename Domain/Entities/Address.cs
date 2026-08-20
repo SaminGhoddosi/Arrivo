fc7f754a6f8a48091091c357e2873813aa326039
Samin Ghoddosi <saminghoddosi44@gmail.com>
2026-08-20 16:21:40 -0300

    Refactor Address and Package domain structure

--- Domain/Entities/Address.cs
+++ Domain/Entities/Address.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entities
+﻿namespace Domain.Entities
 {
-    public class Address : Entity
+    public class Address
     {
         public int ZipCode { get; set; }
         public string City { get; set; }
--- Domain/Entities/Building.cs
+++ Domain/Entities/Building.cs
@@ -9,7 +9,6 @@ namespace Domain.Entities
     public class Building : Entity
     {
         public string Name { get; set; }
-        public int AddressId { get; set; }
         public Address Address { get; set; }
     }
 }
